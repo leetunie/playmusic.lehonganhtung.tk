412078e77b84568fb6d4c99eb50ecf39bdd33588
leTungXDevelopervn <109446582+leTungXDevelopervn@users.noreply.github.com>
2023-01-08 12:46:34 +0000

    Upload
    Upload

---
+++ data/songPlaylists.js
@@ -0,0 +1,403 @@
+
+var listSong = [
+    [
+
+        {
+            name: "Kẻ Theo Đuổi Ánh Sáng",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song1.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "Way Back Home",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song2.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "2AM",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song3.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "Alone Lofi",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song4.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "Anh Sẽ Đợi",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song5.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "Chạy Khỏi Thế Giới Này",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song6.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "EDM Nightcore",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song7.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "Tie Me Down",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song8.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "Cute Song",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song9.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "Lời Anh Chưa Thể Nói Ra",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song10.flac",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "Death Bed",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song11.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "Cứ Nói Yêu Lần Này",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song12.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "Save Me",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song13.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "Savannah",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song14.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "Hey Hey Hey x Mood",
+            singer: ['Lê Hồng Anh Tùng'],
+            path: "./assets/music/listSong1/song15.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "Gu",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song16.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "Let Her Go",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song17.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "Love Story",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song18.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "Lemon",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song19.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "EDM Nightcore",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song20.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "Nhỏ Lớp Trưởng",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song21.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Simple Love❤",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song22.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Lời Yêu Ngây Dại",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song23.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Where We Started",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song24.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Renai Circulation恋愛サーキュレーション歌ってみたなみりん",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song25.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Seasons",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song26.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Senbonzakura Violin  Lindsey Stirling",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song27.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Sparkle",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song28.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Never Be Alone",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song29.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Close To The Sun",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song30.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Fly Away",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song31.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Yêu Sắc Yếu",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song32.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Độ Tộc ",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song33.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+        {
+            name: "Rồi Tới Luôn",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song34.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Yêu Là Cưới",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song35.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Chúng Ta Của Hiện Tại",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song36.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Em Không Hiểu",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song37.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Đã Lỡ Yêu Em Nhiều",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song38.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Cưới Đi",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song39.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Ái Nộ",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song40.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Thằng Điên",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song41.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Yêu Đơn Phương",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song42.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Trời Giấu Trời Mang Đi",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song43.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "EX's Hate Me",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song44.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Muộn Rồi Mà Sao Còn",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song45.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "THE PLAYAH",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song46.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Cưới Thôi",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song47.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Mượn Rượu Tỏ Tình",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song48.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Yêu Một Người Có Lẽ",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song49.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Anh Không Đòi Quà",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song50.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Đi Đu Đưa Đi",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song51.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "I Do",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song52.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Reality",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song53.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Attention",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song54.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Monsters",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song55.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Faded",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song56.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Mood",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song57.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Lê Hồng Anh Tùng",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song58.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "The River",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song59.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Rendezvous",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song60.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Way Back",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song61.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Lê Hồng Anh Tùng",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song62.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Stay",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song63.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Lê Hồng Anh Tùng",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song64.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+                {
+            name: "Hero",
+            singer: ["Lê Hồng Anh Tùng"],
+            path: "./assets/music/listSong1/song65.mp3",
+            image: "./assets/img/music/listSong1/chill.gif"
+        },
+
+    ],
+];
+
+const MUSIC_STORAGE_KEY = 'VIK_MUSIC';
+
+localStorage.setItem(MUSIC_STORAGE_KEY, JSON.stringify(listSong));
+
+
